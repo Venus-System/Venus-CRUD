08d8f4c72430b6ced9f14769dd3a37e2f173617d
FelipeAugustoSan <santos.felipeaugustoger@gmail.com>
2026-08-17 09:04:08 -0300

    feat: add review mapper patch methods

--- src/main/java/com/venus/crud/mapper/review/ReviewVoteMapper.java
+++ src/main/java/com/venus/crud/mapper/review/ReviewVoteMapper.java
@@ -1,17 +1,16 @@
 package com.venus.crud.mapper.review;
 
 import com.venus.crud.config.VenusMapperConfig;
+import com.venus.crud.dto.request.review.ReviewVotePatchRequest;
 import com.venus.crud.dto.request.review.ReviewVoteRequest;
 import com.venus.crud.dto.response.review.ReviewVoteResponse;
 import com.venus.crud.entity.review.Review;
 import com.venus.crud.entity.review.ReviewVote;
 import com.venus.crud.entity.user.User;
-import org.mapstruct.BeanMapping;
 import org.mapstruct.InheritConfiguration;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
-import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @Mapper(config = VenusMapperConfig.class)
 public interface ReviewVoteMapper {
@@ -27,8 +26,13 @@ public interface ReviewVoteMapper {
     ReviewVoteResponse toResponse(ReviewVote entity);
 
     @InheritConfiguration(name = "toEntity")
-    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     void updateEntity(ReviewVoteRequest request, @MappingTarget ReviewVote entity);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "review", source = "reviewId")
+    @Mapping(target = "user", source = "userId")
+    void patchEntity(ReviewVotePatchRequest request, @MappingTarget ReviewVote entity);
 
     default Review mapReview(Long reviewId) {
         if (reviewId == null) {
