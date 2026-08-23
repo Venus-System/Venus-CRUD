7a095e8f6f85ad69f851f1b45d3d202fa25067ee
FelipeAugustoSan <santos.felipeaugustoger@gmail.com>
2026-08-23 18:35:01 -0300

    fix: enforce null-ignore strategy on shared patch mappers

--- src/main/java/com/venus/crud/mapper/shared/ProfileTagMapper.java
+++ src/main/java/com/venus/crud/mapper/shared/ProfileTagMapper.java
@@ -5,10 +5,12 @@ import com.venus.crud.dto.request.shared.ProfileTagPatchRequest;
 import com.venus.crud.dto.request.shared.ProfileTagRequest;
 import com.venus.crud.dto.response.shared.ProfileTagResponse;
 import com.venus.crud.entity.shared.ProfileTag;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.InheritConfiguration;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @Mapper(config = VenusMapperConfig.class)
 public interface ProfileTagMapper {
@@ -26,5 +28,6 @@ public interface ProfileTagMapper {
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "createdAt", ignore = true)
     @Mapping(target = "updatedAt", ignore = true)
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     void patchEntity(ProfileTagPatchRequest request, @MappingTarget ProfileTag entity);
 }
