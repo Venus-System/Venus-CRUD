0dc844fa53b50b7dd93b33bbb1adad14d3859024
FelipeAugustoSan <santos.felipeaugustoger@gmail.com>
2026-08-23 18:34:34 -0300

    fix: enforce null-ignore strategy on product patch mappers

--- src/main/java/com/venus/crud/mapper/product/BrandMapper.java
+++ src/main/java/com/venus/crud/mapper/product/BrandMapper.java
@@ -5,10 +5,12 @@ import com.venus.crud.dto.request.product.BrandPatchRequest;
 import com.venus.crud.dto.request.product.BrandRequest;
 import com.venus.crud.dto.response.product.BrandResponse;
 import com.venus.crud.entity.product.Brand;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.InheritConfiguration;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @Mapper(config = VenusMapperConfig.class)
 public interface BrandMapper {
@@ -26,5 +28,6 @@ public interface BrandMapper {
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "createdAt", ignore = true)
     @Mapping(target = "updatedAt", ignore = true)
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     void patchEntity(BrandPatchRequest request, @MappingTarget Brand entity);
 }
--- src/main/java/com/venus/crud/mapper/product/ProductLabelMapper.java
+++ src/main/java/com/venus/crud/mapper/product/ProductLabelMapper.java
@@ -6,10 +6,12 @@ import com.venus.crud.dto.request.product.ProductLabelRequest;
 import com.venus.crud.dto.response.product.ProductLabelResponse;
 import com.venus.crud.entity.product.ProductLabel;
 import com.venus.crud.entity.product.ProductVersion;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.InheritConfiguration;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @Mapper(config = VenusMapperConfig.class)
 public interface ProductLabelMapper {
@@ -30,6 +32,7 @@ public interface ProductLabelMapper {
     @Mapping(target = "createdAt", ignore = true)
     @Mapping(target = "updatedAt", ignore = true)
     @Mapping(target = "productVersion", source = "productVersionId")
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     void patchEntity(ProductLabelPatchRequest request, @MappingTarget ProductLabel entity);
 
     default ProductVersion mapProductVersion(Long productVersionId) {
