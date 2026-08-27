fc1855398138c0744ddb38a80261e06ae791d45b
FelipeAugustoSan <santos.felipeaugustoger@gmail.com>
2026-08-27 20:09:43 -0300

    fix: mark created_at/updated_at as db-generated, read-only columns

--- src/main/java/com/venus/crud/entity/shared/AuditableEntity.java
+++ src/main/java/com/venus/crud/entity/shared/AuditableEntity.java
@@ -3,9 +3,12 @@ package com.venus.crud.entity.shared;
 import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
 import java.time.OffsetDateTime;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.hibernate.annotations.Generated;
+import org.hibernate.generator.EventType;
 
 @Getter
 @Setter
@@ -13,6 +16,8 @@ import lombok.ToString;
 @MappedSuperclass
 public abstract class AuditableEntity extends BaseEntity {
 
-    @Column(name = "updated_at", nullable = false)
+    @Setter(AccessLevel.NONE)
+    @Generated(event = {EventType.INSERT, EventType.UPDATE})
+    @Column(name = "updated_at", nullable = false, insertable = false, updatable = false)
     private OffsetDateTime updatedAt;
 }
