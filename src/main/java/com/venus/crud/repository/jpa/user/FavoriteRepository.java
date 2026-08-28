a878d0cb1eafe33de44056f0a9ee70897ad2b6eb
FelipeAugustoSan <santos.felipeaugustoger@gmail.com>
2026-08-28 08:20:05 -0300

    feat: add unpaginated findByUserId to FavoriteRepository

--- src/main/java/com/venus/crud/repository/jpa/user/FavoriteRepository.java
+++ src/main/java/com/venus/crud/repository/jpa/user/FavoriteRepository.java
@@ -1,6 +1,7 @@
 package com.venus.crud.repository.jpa.user;
 
 import com.venus.crud.entity.user.Favorite;
+import java.util.List;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface FavoriteRepository extends JpaRepository<Favorite, Long> {
 
+    List<Favorite> findByUserId(Long userId);
     Slice<Favorite> findByUserId(Long userId, Pageable pageable);
     Slice<Favorite> findByProductId(Long productId, Pageable pageable);
     boolean existsByUserIdAndProductId(Long userId, Long productId);
