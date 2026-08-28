b028b514711e2b1b047951a05d30f67d9b9a5ae2
FelipeAugustoSan <santos.felipeaugustoger@gmail.com>
2026-08-28 08:10:49 -0300

    refactor: convert Allergy repository queries from Page to Slice

--- src/main/java/com/venus/crud/repository/jpa/user/AllergyRepository.java
+++ src/main/java/com/venus/crud/repository/jpa/user/AllergyRepository.java
@@ -2,8 +2,8 @@ package com.venus.crud.repository.jpa.user;
 
 import com.venus.crud.entity.enums.AllergyType;
 import com.venus.crud.entity.user.Allergy;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -14,5 +14,6 @@ public interface AllergyRepository extends JpaRepository<Allergy, Long> {
 
     Optional<Allergy> findByAllergyName(String allergyName);
     boolean existsByAllergyName(String allergyName);
-    Page<Allergy> findByAllergyType(AllergyType allergyType, Pageable pageable);
+    Slice<Allergy> findByAllergyType(AllergyType allergyType, Pageable pageable);
+    Slice<Allergy> findAllBy(Pageable pageable);
 }
