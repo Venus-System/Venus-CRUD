be81df827e56402ee7bfd74c7c028d563f44b3c3
FelipeAugustoSan <santos.felipeaugustoger@gmail.com>
2026-08-28 08:20:06 -0300

    feat: add unpaginated findByUserId to UserListRepository

--- src/main/java/com/venus/crud/repository/jpa/user/UserListRepository.java
+++ src/main/java/com/venus/crud/repository/jpa/user/UserListRepository.java
@@ -2,6 +2,7 @@ package com.venus.crud.repository.jpa.user;
 
 import com.venus.crud.entity.enums.ListType;
 import com.venus.crud.entity.user.UserList;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserListRepository extends JpaRepository<UserList, Long> {
 
+    List<UserList> findByUserId(Long userId);
     Slice<UserList> findByUserId(Long userId, Pageable pageable);
     Slice<UserList> findByUserIdAndListType(Long userId, ListType listType, Pageable pageable);
     Optional<UserList> findByUserIdAndName(Long userId, String name);
