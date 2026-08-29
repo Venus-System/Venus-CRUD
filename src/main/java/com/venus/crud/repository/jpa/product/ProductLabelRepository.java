a50f3d5078c706e950e0a305d2138fb870ece790
FelipeAugustoSan <santos.felipeaugustoger@gmail.com>
2026-08-29 20:06:28 -0300

    refactor: align ProductLabelRepository with project query pattern

--- src/main/java/com/venus/crud/repository/jpa/product/ProductLabelRepository.java
+++ src/main/java/com/venus/crud/repository/jpa/product/ProductLabelRepository.java
@@ -2,8 +2,8 @@ package com.venus.crud.repository.jpa.product;
 
 import com.venus.crud.entity.enums.SourceType;
 import com.venus.crud.entity.product.ProductLabel;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -13,7 +13,10 @@ import java.util.Optional;
 public interface ProductLabelRepository extends JpaRepository<ProductLabel, Long> {
 
     Optional<ProductLabel> findByProductVersionId(Long productVersionId);
-    Page<ProductLabel> findByLanguage(String language, Pageable pageable);
-    Page<ProductLabel> findBySourceType(SourceType sourceType, Pageable pageable);
+    boolean existsByProductVersionId(Long productVersionId);
+    void deleteByProductVersionId(Long productVersionId);
     Optional<ProductLabel> findBySourceReference(String sourceReference);
+    Slice<ProductLabel> findByLanguage(String language, Pageable pageable);
+    Slice<ProductLabel> findBySourceType(SourceType sourceType, Pageable pageable);
+    Slice<ProductLabel> findAllBy(Pageable pageable);
 }
