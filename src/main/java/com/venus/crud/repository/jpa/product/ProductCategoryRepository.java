7409e0649a50b22206cf6e233a8af5c0869032b2
FelipeAugustoSan <santos.felipeaugustoger@gmail.com>
2026-08-29 19:51:56 -0300

    refactor: align ProductCategoryRepository with project query pattern

--- src/main/java/com/venus/crud/repository/jpa/product/ProductCategoryRepository.java
+++ src/main/java/com/venus/crud/repository/jpa/product/ProductCategoryRepository.java
@@ -1,6 +1,8 @@
 package com.venus.crud.repository.jpa.product;
 
 import com.venus.crud.entity.product.ProductCategory;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,5 +12,6 @@ import java.util.Optional;
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 
     Optional<ProductCategory> findByNameIgnoreCase(String name);
-    boolean existsByNameIgnoreCase(String name);
+    Slice<ProductCategory> findByNameContainingIgnoreCase(String name, Pageable pageable);
+    Slice<ProductCategory> findAllBy(Pageable pageable);
 }
