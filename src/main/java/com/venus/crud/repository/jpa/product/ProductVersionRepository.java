483d0ee386c292736752bd454d2962b1ec492e0f
FelipeAugustoSan <santos.felipeaugustoger@gmail.com>
2026-08-29 19:52:09 -0300

    refactor: align ProductVersionRepository with project query pattern

--- src/main/java/com/venus/crud/repository/jpa/product/ProductVersionRepository.java
+++ src/main/java/com/venus/crud/repository/jpa/product/ProductVersionRepository.java
@@ -2,8 +2,8 @@ package com.venus.crud.repository.jpa.product;
 
 import com.venus.crud.entity.enums.VersionStatus;
 import com.venus.crud.entity.product.ProductVersion;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -14,7 +14,9 @@ import java.util.Optional;
 public interface ProductVersionRepository extends JpaRepository<ProductVersion, Long> {
 
     List<ProductVersion> findByProductId(Long productId);
+    Slice<ProductVersion> findByProductId(Long productId, Pageable pageable);
     Optional<ProductVersion> findByProductIdAndIsCurrentTrue(Long productId);
-    Page<ProductVersion> findByStatus(VersionStatus status, Pageable pageable);
-    List<ProductVersion> findByFormulaSignature(String formulaSignature);
+    Slice<ProductVersion> findByStatus(VersionStatus status, Pageable pageable);
+    Slice<ProductVersion> findByFormulaSignature(String formulaSignature, Pageable pageable);
+    Slice<ProductVersion> findAllBy(Pageable pageable);
 }
