52d98d89b377f98603f1d523e11d82710c8eada1
FelipeAugustoSan <santos.felipeaugustoger@gmail.com>
2026-08-30 19:14:21 -0300

    feat: add findByProductVersionIdAndIngredientId and switch queries to Slice

--- src/main/java/com/venus/crud/repository/jpa/ingredient/ProductIngredientRepository.java
+++ src/main/java/com/venus/crud/repository/jpa/ingredient/ProductIngredientRepository.java
@@ -1,20 +1,23 @@
 package com.venus.crud.repository.jpa.ingredient;
 
 import com.venus.crud.entity.ingredient.ProductIngredient;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ProductIngredientRepository extends JpaRepository<ProductIngredient, Long> {
 
     @EntityGraph(attributePaths = "ingredient")
     List<ProductIngredient> findByProductVersionIdOrderByPosition(Long productVersionId);
-    Page<ProductIngredient> findByIngredientId(Long ingredientId, Pageable pageable);
+    Slice<ProductIngredient> findByIngredientId(Long ingredientId, Pageable pageable);
+    Optional<ProductIngredient> findByProductVersionIdAndIngredientId(Long productVersionId, Long ingredientId);
     boolean existsByProductVersionIdAndIngredientId(Long productVersionId, Long ingredientId);
     void deleteByProductVersionIdAndIngredientId(Long productVersionId, Long ingredientId);
+    Slice<ProductIngredient> findAllBy(Pageable pageable);
 }
