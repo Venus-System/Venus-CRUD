45d4ec5ec73912ff9a63dab86cf91951f5326395
FelipeAugustoSan <santos.felipeaugustoger@gmail.com>
2026-08-31 11:15:23 -0300

    feat: add name search and pagination to ScoringModelRepository

--- src/main/java/com/venus/crud/repository/jpa/scoring/ScoringModelRepository.java
+++ src/main/java/com/venus/crud/repository/jpa/scoring/ScoringModelRepository.java
@@ -1,6 +1,8 @@
 package com.venus.crud.repository.jpa.scoring;
 
 import com.venus.crud.entity.scoring.ScoringModel;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,5 +13,6 @@ public interface ScoringModelRepository extends JpaRepository<ScoringModel, Long
 
     Optional<ScoringModel> findByIsActiveTrue();
     Optional<ScoringModel> findByNameAndVersion(String name, String version);
-    boolean existsByNameAndVersion(String name, String version);
+    Slice<ScoringModel> findByNameContainingIgnoreCase(String name, Pageable pageable);
+    Slice<ScoringModel> findAllBy(Pageable pageable);
 }
