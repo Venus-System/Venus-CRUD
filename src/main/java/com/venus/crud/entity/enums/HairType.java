c0fe924a52652073410324d8eb7cd10acad7454d
FelipeAugustoSan <santos.felipeaugustoger@gmail.com>
2026-09-14 21:45:55 -0300

    feat: expandindo HairType para os 10 niveis da escala Andre Walker

--- src/main/java/com/venus/crud/entity/enums/HairType.java
+++ src/main/java/com/venus/crud/entity/enums/HairType.java
@@ -1,9 +1,14 @@
 package com.venus.crud.entity.enums;
 
 public enum HairType {
-    STRAIGHT,
-    WAVY,
-    CURLY,
-    COILY,
-    OTHER
+    TYPE_1,
+    TYPE_2A,
+    TYPE_2B,
+    TYPE_2C,
+    TYPE_3A,
+    TYPE_3B,
+    TYPE_3C,
+    TYPE_4A,
+    TYPE_4B,
+    TYPE_4C
 }
