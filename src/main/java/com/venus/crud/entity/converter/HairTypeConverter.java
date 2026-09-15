5e3ec7596fc7ac99d62fc6f80018c8bcd4f05c84
FelipeAugustoSan <santos.felipeaugustoger@gmail.com>
2026-09-14 21:45:55 -0300

    feat: ajustando HairTypeConverter para os 10 niveis novos de HairType

--- src/main/java/com/venus/crud/entity/converter/HairTypeConverter.java
+++ src/main/java/com/venus/crud/entity/converter/HairTypeConverter.java
@@ -1,11 +1,21 @@
 package com.venus.crud.entity.converter;
 
 import com.venus.crud.entity.enums.HairType;
+import jakarta.persistence.AttributeConverter;
 import jakarta.persistence.Converter;
 
 @Converter(autoApply = true)
-public class HairTypeConverter extends LowercaseEnumConverter<HairType> {
-    public HairTypeConverter() {
-        super(HairType.class);
+public class HairTypeConverter implements AttributeConverter<HairType, String> {
+
+    private static final String PREFIX = "TYPE_";
+
+    @Override
+    public String convertToDatabaseColumn(HairType attribute) {
+        return attribute == null ? null : attribute.name().substring(PREFIX.length());
+    }
+
+    @Override
+    public HairType convertToEntityAttribute(String dbData) {
+        return dbData == null ? null : HairType.valueOf(PREFIX + dbData);
     }
 }
